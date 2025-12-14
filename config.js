7fc20cdf98f7e4771a60cf23efe3771c6ab66698
popokoko <30921662+popokoko@users.noreply.github.com>
2025-12-14 08:32:21 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// Firebase Configuration
+// This file centralizes the configuration for the entire application.
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyB5omg8yaumVDlvd8pGxub0EyX8SW2KifE",
+    authDomain: "peacelove-27b0d.firebaseapp.com",
+    projectId: "peacelove-27b0d",
+    storageBucket: "peacelove-27b0d.firebasestorage.app",
+    messagingSenderId: "888936275412",
+    appId: "1:888936275412:web:a0852a693d01cc1884ca41"
+};
